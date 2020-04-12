00713d2872ea65a7b441ed169815d195bceaee1a
limingyu <limingyu@dip-ai>
2020-04-12 23:25:56 +0800

    税务入库修改

--- src/page/dataWarehousing/taxWarehousing/index.vue
+++ src/page/dataWarehousing/taxWarehousing/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="wrapper">
     <div class="flex-right">
-      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form :inline="true" :model="searchForm" class="demo-form-inline">
         <el-form-item label="所属税期：">
-          <el-input size="mini" v-model="formInline.taxPeriod" placeholder="请输入"></el-input>
+          <el-date-picker
+            v-model="searchForm.taxPeriod"
+            size="mini"
+            type="month"
+            value-format="yyyy-MM"
+            placeholder="选择日期">
+          </el-date-picker>
         </el-form-item>
         <el-form-item label="所属区：">
-          <el-select v-model="formInline.taxPeriod" placeholder="请选择" size="mini">
+          <el-select v-model="searchForm.belongsArea" placeholder="请选择" size="mini">
             <el-option
-              v-for="item in options"
+              v-for="item in belongsAreaOptions"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -16,9 +22,9 @@
           </el-select>
         </el-form-item>
         <el-form-item label="税款所属 税务机关:">
-          <el-select v-model="formInline.taxPeriod" placeholder="请选择" size="mini">
+          <el-select v-model="searchForm.taxCollectionAuthority" placeholder="请选择" size="mini">
             <el-option
-              v-for="item in options"
+              v-for="item in taxCollectionAuthorityOptions"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -26,9 +32,9 @@
           </el-select>
         </el-form-item>
         <el-form-item label="收款国库：">
-          <el-select v-model="formInline.taxPeriod" placeholder="请选择" size="mini">
+          <el-select v-model="searchForm.receiveTreasury" placeholder="请选择" size="mini">
             <el-option
-              v-for="item in options"
+              v-for="item in receiveTreasuryOptions"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -36,9 +42,9 @@
           </el-select>
         </el-form-item>
         <el-form-item label="征收品目：">
-          <el-select v-model="formInline.taxPeriod" placeholder="请选择" size="mini">
+          <el-select v-model="searchForm.collectionItems" placeholder="请选择" size="mini">
             <el-option
-              v-for="item in options"
+              v-for="item in collectionItemsOptions"
               :key="item.value"
               :label="item.label"
               :value="item.value">
@@ -51,7 +57,6 @@
       </el-form>
       <div class="operation_btns">
         <el-button size="mini" type="warning">xls导入</el-button>
-        <!-- <el-button size="mini" type="warning" @click="clickAnalysis">分析（足额）</el-button> -->
       </div>
       <el-table :data="tableData" stripe style="width: 100%">
         <el-table-column prop="taxPeriod" label="所属税期"></el-table-column>
@@ -76,157 +81,145 @@
         <el-table-column label="操作" width="200">
           <template slot-scope="scope">
             <el-button size="mini" type="warning"
-              @click="handleEdit(scope.$index, scope.row)">确认提交</el-button>
+                       @click="handleEdit(scope.$index, scope.row)">确认提交</el-button>
             <el-button size="mini" type="warning"
-              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                       @click="handleDelete(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
       <el-pagination
-      style="margin: 15px 0"
-      @current-change="handleCurrentChange" 
-      :current-page.sync="page.currPage"
-      :page-size="page.pageSize"
-      layout="prev, pager, next, jumper"
-      :total="page.totalPage">
-    </el-pagination>
-    <!-- 弹窗 -->
-    <DialogCommon 
-    :centerText="centerText" 
-    :centerDialogVisible="centerDialogVisible" 
-    @delDialog="sureDelDialog"
-    @cancleDialog="cancleDelDialog"></DialogCommon>
+        style="margin: 15px 0"
+        @current-change="handleCurrentChange"
+        :current-page.sync="page.currPage"
+        :page-size="page.pageSize"
+        layout="prev, pager, next, jumper"
+        :total="page.totalCount">
+      </el-pagination>
+      <!-- 弹窗 -->
+      <DialogCommon
+        :centerText="centerText"
+        :centerDialogVisible="centerDialogVisible"
+        @delDialog="sureDelDialog"
+        @cancleDialog="centerDialogVisible = false"></DialogCommon>
     </div>
   </div>
 </template>
 <script>
-import DialogCommon from '@/components/dialogCommon';
-import {dataStorageApis} from '@/http/api'
-export default {
-  components: {
-    DialogCommon
-  },
-  data() {
-    return {
-      formInline: {
-        "taxPeriod": "", //税期
-        "collectionItems": "", //征收品目
-        "belongsArea": "", //所属区
-        "receiveTreasury": "", // 收款国库
-        "compCode": "", //社会信用代码（纳税人识别号）
-        "taxPayer": "", // 那随人名称
-        "taxCollectionAuthority":""// 征收税务机关
-      },
-      options: [{
-        value: '选项1',
-        label: '黄金糕'
-      }, {
-        value: '选项2',
-        label: '双皮奶'
-      }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
-      }],
-      value: '',
-      tableData: [],
-      page:{
-        currPage:1, // 当前页
-        pageSize: 10, // 每页条数
-        totalPage: 100, // 总页数
-      },
-      centerText: '是否确定删除该税务入库信息？',
-      centerDialogVisible: false,
-      deleteId: ''
-    }
-  },
-  mounted(){
-    this.postStoreStoretaxtempList()
-  },
-  methods: {
-    // 列表
-    postStoreStoretaxtempList(){
-      let data = {
-        "taxPeriod": this.formInline.taxPeriod, //税期
-        "collectionItems": this.formInline.collectionItems, //征收品目
-        "belongsArea": this.formInline.belongsArea, //所属区
-        "receiveTreasury": this.formInline.receiveTreasury, // 收款国库
-        "compCode": this.formInline.compCode, //社会信用代码（纳税人识别号）
-        "taxPayer": this.formInline.taxPayer, // 那随人名称
-        "taxCollectionAuthority": this.formInline.taxCollectionAuthority,// 征收税务机关
-        "currPage":this.page.currPage,
-        "pageSize":this.page.pageSize,
-      }
-      dataStorageApis.postStoreStoretaxtempList(data).then( res=> {
-        this.tableData = res.result.list
-        this.page.totalPage = res.result.totalCount
-      })
-    },
-    // 足额分析
-    postStoreStoretaxtempHasfulllist(){
-      let data = {
-        "taxPeriod": this.formInline.taxPeriod, //税期
-        "collectionItems": this.formInline.collectionItems, //征收品目
-        "belongsArea": this.formInline.belongsArea, //所属区
-        "receiveTreasury": this.formInline.receiveTreasury, // 收款国库
-        "compCode": this.formInline.compCode, //社会信用代码（纳税人识别号）
-        "taxPayer": this.formInline.taxPayer, // 那随人名称
-        "taxCollectionAuthority": this.formInline.taxCollectionAuthority,// 征收税务机关
-        "currPage":this.page.currPage,
-        "pageSize":this.page.pageSize,
-      }
-      dataStorageApis.postStoreStoretaxtempHasfulllist(data).then( res=> {
-        this.tableData = res.result.list
-        this.page.totalPage = res.result.totalCount
-      })
-    },
-    onSubmit() {
-      this.page.currPage = 1
-      this.tableData = []
-      this.postStoreStoretaxtempList()
-    },
-    // 分析足额
-    clickAnalysis(){
-      this.tableData = []
-      this.postStoreStoretaxtempHasfulllist()
-    },
-    handleEdit(index, row) {
-      this.$router.push({
-        path: `/taxWarehousingInfoHandle?id=${row.id}`
-      })
-    },
-    handleDelete(index, row) {
-      this.centerDialogVisible = true
-      this.deleteId = row.id
-    },
-    sureDelDialog(){
-      this.centerDialogVisible = false
-      dataStorageApis.postStoreStoretaxtempDelete({
-        ids: this.deleteId
-      }).then(res => {
-        if (res.status == 200) {
-          this.$message.success('删除成功！');
-          this.page.currPage = 1
-          this.tableData = []
-          this.postStoreStoretaxtempList()
+    import DialogCommon from '@/components/dialogCommon';
+    import {dataStorageApis, commonApi} from '@/http/api'
+    export default {
+        components: {
+            DialogCommon
+        },
+        data() {
+            return {
+                searchForm: {
+                    "taxPeriod": "", //税期
+                    "collectionItems": "", //征收品目
+                    "belongsArea": "", //所属区
+                    "receiveTreasury": "", // 收款国库
+                    "taxCollectionAuthority":""// 征收税务机关
+                },
+                belongsAreaOptions: [], // 所属区下拉选项
+                taxCollectionAuthorityOptions: [], // 税务机关下拉选项
+                receiveTreasuryOptions: [], // 收款国库下拉选项
+                collectionItemsOptions: [], // 征收品目下拉选项
+                tableData: [],
+                page:{
+                    currPage:1, // 当前页
+                    pageSize: 10, // 每页条数
+                    totalCount: 100, // 总页数
+                },
+                centerText: '是否确定删除该税务入库信息？',
+                centerDialogVisible: false,
+                deleteId: ''
+            }
+        },
+        mounted(){
+            this.postStoreStoretaxtempList()
+            this.getDataDic()
+        },
+        methods: {
+            // 获取字典
+            getDataDic() {
+                commonApi.getDataDic('belongsArea').then(res => {
+                    if (res.status === 200) {
+                        this.belongsAreaOptions = res.result
+                    }
+                })
+                commonApi.getDataDic('taxCollectionAuthority').then(res => {
+                    if (res.status === 200) {
+                        this.taxCollectionAuthorityOptions = res.result
+                    }
+                })
+                commonApi.getDataDic('receiveTreasury').then(res => {
+                    if (res.status === 200) {
+                        this.receiveTreasuryOptions = res.result
+                    }
+                })
+                commonApi.getDataDic('collectionItems').then(res => {
+                    if (res.status === 200) {
+                        this.collectionItemsOptions = res.result
+                    }
+                })
+            },
+            // 列表
+            postStoreStoretaxtempList(){
+                let data = {
+                    ...this.searchForm,
+                    taxPeriod: this.searchForm.taxPeriod.split('-').join(''),
+                    ...this.page,
+                }
+                dataStorageApis.postStoreStoretaxtempList(data).then( res=> {
+                    if (res.status === 200) {
+                        this.tableData = res.result.list
+                        this.page = {
+                            ...this.page,
+                            pageSize: res.result.pageSize,
+                            totalCount: res.result.totalCount,
+                            currPage: res.result.currPage
+                        }
+                    } else {
+                        this.$message.error(res.message)
+                    }
+                })
+            },
+            // 数据删除按钮
+            handleDelete(row) {
+                this.centerDialogVisible = true
+                this.deleteId = row.id
+            },
+            // 删除弹窗确认
+            sureDelDialog(){
+                this.centerDialogVisible = false
+                dataStorageApis.postStoreStoretaxtempDelete({
+                    ids: this.deleteId
+                }).then(res => {
+                    if (res.status == 200) {
+                        this.$message.success('删除成功！');
+                        this.postStoreStoretaxtempList()
+                    }
+                })
+            },
+            // 列表翻页
+            handleCurrentChange(val) {
+                console.log('val', val)
+                this.page.currPage = val
+                this.postStoreStoretaxtempList()
+            },
+
+            onSubmit() {
+                this.page.currPage = 1
+                this.tableData = []
+                this.postStoreStoretaxtempList()
+            },
+            handleEdit(index, row) {
+                this.$router.push({
+                    path: `/taxWarehousingInfoHandle?id=${row.id}`
+                })
+            },
         }
-      })
-    },
-    cancleDelDialog(){
-      this.centerDialogVisible = false
-    },
-    handleCurrentChange(val) {
-      this.tableData = []
-      this.page.currPage = val
-      this.postStoreStoretaxtempList()
     }
-  }
-}
 </script>
 <style lang="scss" scoped>
 
