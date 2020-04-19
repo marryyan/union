f8785d1f92e14a54617f03007c836dc8416c9105
limingyu <limingyu@dip-ai>
2020-04-19 21:41:08 +0800

    bbbbbbbbbb

--- src/http/distributionManagementApi.js
+++ src/http/distributionManagementApi.js
@@ -4,7 +4,11 @@
 import { postApi, getApi } from "./http";
 
 const distributionManagementApi = {
-
+  // 分配管理 工会管理分页
+  postStoreTaxdistributionList: function postStoreTaxdistributionList (data){
+    const url = `/union/store/taxdistribution/list?dicConvert=1`;
+    return postApi(url, data)
+  },
 }
 
 export default distributionManagementApi
--- src/page/assignManage/tradeUnion.vue
+++ src/page/assignManage/tradeUnion.vue
@@ -5,65 +5,56 @@
         <el-form-item label="税款所属期：">
           <el-date-picker
             size="mini"
-            v-model="searchForm.proDate"
+            v-model="searchForm.taxPeriod"
             type="date"
             value-format="yyyy-MM-dd"
             placeholder="选择日期">
           </el-date-picker>
         </el-form-item>
-        <el-form-item label="征收品目：">
-          <el-select size="mini" v-model="searchForm.user" placeholder="请输入">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-        </el-form-item>
         <el-form-item label="税款所属 税务机关：">
-          <el-select size="mini" v-model="searchForm.user" placeholder="请输入">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
+          <el-input size="mini" v-model="searchForm.taxBelongsComp" placeholder="请输入"></el-input>
+<!--          <el-select size="mini" v-model="searchForm.taxBelongsComp" placeholder="请输入">-->
+<!--            <el-option-->
+<!--              v-for="item in taxBelongsCompOptions"-->
+<!--              :key="item.k"-->
+<!--              :label="item.v"-->
+<!--              :value="item.k">-->
+<!--            </el-option>-->
+<!--          </el-select>-->
         </el-form-item>
         <el-form-item label="企业名称：">
-          <el-input size="mini" v-model="searchForm.accountName" placeholder="请输入"></el-input>
+          <el-input size="mini" v-model="searchForm.taxPayer" placeholder="请输入"></el-input>
         </el-form-item>
         <el-form-item label="社会统一信用代码：">
-          <el-input size="mini" v-model="searchForm.accountName" placeholder="请输入"></el-input>
+          <el-input size="mini" v-model="searchForm.compCode" placeholder="请输入"></el-input>
         </el-form-item>
         <el-form-item label="分配状态：">
-          <el-select size="mini" v-model="searchForm.user" placeholder="请输入">
+          <el-select size="mini" v-model="searchForm.distributionType" placeholder="请输入">
             <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
+              v-for="item in distributionTypeOptions"
+              :key="item.k"
+              :label="item.v"
+              :value="item.k">
             </el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="缴费类型：">
-          <el-select size="mini" v-model="searchForm.user" placeholder="请输入">
+          <el-select size="mini" v-model="searchForm.collectionItemsCode" placeholder="请输入">
             <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
+              v-for="item in collectionItemsCodeOptions"
+              :key="item.k"
+              :label="item.v"
+              :value="item.k">
             </el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="工会类别：">
-          <el-select size="mini" v-model="searchForm.user" placeholder="请输入">
+          <el-select size="mini" v-model="searchForm.unionType" placeholder="请输入">
             <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
+              v-for="item in unionTypeOptions"
+              :key="item.k"
+              :label="item.v"
+              :value="item.k">
             </el-option>
           </el-select>
         </el-form-item>
@@ -75,31 +66,31 @@
         <el-button size="mini" type="warning">分配(下载打印)</el-button>
       </div>
       <el-table :data="tableData" stripe style="width: 100%">
-        <el-table-column prop="name" label="所属税期"></el-table-column>
-        <el-table-column prop="name" label="所属区"></el-table-column>
-        <el-table-column prop="name" label="社会信用代码（纳税人识别号）" width="180"></el-table-column>
-        <el-table-column prop="name" label="纳税人名称" width="180"></el-table-column>
-        <el-table-column prop="name" label="工会经费编码" width="180"></el-table-column>
-        <el-table-column prop="name" label="工会开户行" width="180"></el-table-column>
-        <el-table-column prop="name" label="工会银行账号" width="180"></el-table-column>
-        <el-table-column prop="name" label="工会开户名称" width="180"></el-table-column>
-        <el-table-column prop="name" label="计税依据"></el-table-column>
-        <el-table-column prop="name" label="税率"></el-table-column>
-        <el-table-column prop="name" label="实缴金额"></el-table-column>
-        <el-table-column prop="name" label="企业认定"></el-table-column>
-        <el-table-column prop="name" label="工会类别"></el-table-column>
-        <el-table-column prop="name" label="缴费类型"></el-table-column>
-        <el-table-column prop="name" label="省总分配比例" width="180"></el-table-column>
-        <el-table-column prop="name" label="省总分配金额" width="180"></el-table-column>
-        <el-table-column prop="name" label="市总分配比例" width="180"></el-table-column>
-        <el-table-column prop="name" label="市总分配金额" width="180"></el-table-column>
-        <el-table-column prop="name" label="手续费"></el-table-column>
-        <el-table-column prop="name" label="手续费金额" width="180"></el-table-column>
-        <el-table-column prop="name" label="区县/产业分配比例" width="180"></el-table-column>
-        <el-table-column prop="name" label="区县/产业分配金额" width="180"></el-table-column>
-        <el-table-column prop="name" label="企业分配比例" width="180"></el-table-column>
-        <el-table-column prop="name" label="企业分配金额" width="180"></el-table-column>
-        <el-table-column prop="name" label="分配状态"></el-table-column>
+        <el-table-column prop="taxPeriod" label="所属税期"></el-table-column>
+        <el-table-column prop="belongsAreaName" label="所属区"></el-table-column>
+        <el-table-column prop="compCode" label="社会信用代码（纳税人识别号）" width="180"></el-table-column>
+        <el-table-column prop="taxPayer" label="纳税人名称" width="180"></el-table-column>
+        <el-table-column prop="unionFundCode" label="工会经费编码" width="180"></el-table-column>
+        <el-table-column prop="unionBank" label="工会开户行" width="180"></el-table-column>
+        <el-table-column prop="unionBankAccount" label="工会银行账号" width="180"></el-table-column>
+        <el-table-column prop="unionAccount" label="工会开户名称" width="180"></el-table-column>
+        <el-table-column prop="taxBasis" label="计税依据"></el-table-column>
+        <el-table-column prop="taxRate" label="税率"></el-table-column>
+        <el-table-column prop="paidAmount" label="实缴金额"></el-table-column>
+        <el-table-column prop="compFirmlyType" label="企业认定"></el-table-column>
+        <el-table-column prop="unionType" label="工会类别"></el-table-column>
+        <el-table-column prop="collectionItemsCode" label="缴费类型"></el-table-column>
+        <el-table-column prop="provincePercent" label="省总分配比例" width="180"></el-table-column>
+        <el-table-column prop="provinceMoney" label="省总分配金额" width="180"></el-table-column>
+        <el-table-column prop="cityPercent" label="市总分配比例" width="180"></el-table-column>
+        <el-table-column prop="cityMoney" label="市总分配金额" width="180"></el-table-column>
+        <el-table-column prop="serviceChargePercent" label="手续费"></el-table-column>
+        <el-table-column prop="serviceChargeMoney" label="手续费金额" width="180"></el-table-column>
+        <el-table-column prop="areaIndustryPercent" label="区县/产业分配比例" width="180"></el-table-column>
+        <el-table-column prop="areaIndustryMoney" label="区县/产业分配金额" width="180"></el-table-column>
+        <el-table-column prop="compPercent" label="企业分配比例" width="180"></el-table-column>
+        <el-table-column prop="compMoney" label="企业分配金额" width="180"></el-table-column>
+        <el-table-column prop="distributionType" label="分配状态"></el-table-column>
       </el-table>
       <el-pagination
         style="margin: 15px 0"
@@ -113,41 +104,92 @@
   </div>
 </template>
 <script>
+  import { distributionManagementApis, commonApi } from '@/http/api'
   export default {
     data() {
       return {
           searchForm: {
-
+              taxPeriod: '',
+              taxBelongsComp: '',
+              collectionItemsCode: '',
+              taxPayer: '',
+              compCode: '',
+              distributionType: '',
+              dataType: 1,
+              unionType: ''
           },
           page:{
               currPage: 1, // 当前页
               pageSize: 10, // 每页条数
-              totalPage: 100, // 总页数
+              totalPage: 0, // 总页数
           },
           tableData: [],
         formInline: {
           user: '',
           name: ''
         },
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }]
+          taxBelongsCompOptions: [],
+          unionTypeOptions: [],
+          collectionItemsCodeOptions: [],
+          distributionTypeOptions: [],
+          compFirmlyTypeOptions: [],
       }
     },
+      mounted() {
+        this.getDataDic()
+        this.postStoreTaxdistributionList()
+      },
     methods: {
+        getDataDic() {
+            commonApi.getDataDic('unionType').then(res => {
+                if (res.status === 200) {
+                    this.unionTypeOptions = res.result
+                } else {
+                    this.$message.error(res.message)
+                }
+            })
+            commonApi.getDataDic('collectionItemsCode').then(res => {
+                if (res.status === 200) {
+                    this.collectionItemsCodeOptions = res.result
+                } else {
+                    this.$message.error(res.message)
+                }
+            })
+            commonApi.getDataDic('distributionType').then(res => {
+                if (res.status === 200) {
+                    this.distributionTypeOptions = res.result
+                } else {
+                    this.$message.error(res.message)
+                }
+            })
+            commonApi.getDataDic('compFirmlyType').then(res => {
+                if (res.status === 200) {
+                    this.compFirmlyTypeOptions = res.result
+                } else {
+                    this.$message.error(res.message)
+                }
+            })
+        },
+        postStoreTaxdistributionList() {
+            const data = {
+                ...this.searchForm,
+                ...this.page
+            }
+          distributionManagementApis.postStoreTaxdistributionList(data).then(res => {
+              if (res.status === 200) {
+                  const { list, pageSize, totalPage, currPage } = res.result
+                  this.page = {
+                      ...this.page,
+                      totalPage,
+                      currPage,
+                      pageSize,
+                  }
+                  this.tableData = list
+              } else {
+                  this.$message.error(res.message)
+              }
+          })
+        },
       onSubmit() {
         console.log('submit!');
       },
